8a8d2b0771c7e3618f4ef1fc9f60225d7dbf167d
AT <adriangtamas@gmail.com>
2017-07-18 18:54:07 +0300

    ST02 UI Buttons

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadNextScene()
+	{
+		//Load Scene
+		int currentIndex = SceneManager.GetActiveScene().buildIndex;
+		// Load currentIndex + 1
+		SceneManager.LoadScene(currentIndex + 1);
+	}
+
+	public void Quit()
+	{
+		print("Game will quit");
+		Application.Quit();
+	}
+}
