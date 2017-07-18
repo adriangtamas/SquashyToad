2ef589c857bf3bb635d011c009513d0ad970cf8a
AT <adriangtamas@gmail.com>
2017-07-18 21:29:40 +0300

    ST05 - Debugging forward vectors

--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -12,6 +12,12 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.DrawRay(transform.position, jumpVector, Color.green);
+		var projectedJumpVector = Vector3.ProjectOnPlane(jumpVector, Vector3.up);
+		Debug.DrawRay(transform.position, projectedJumpVector, Color.blue);
+		var radiansToRotate = Mathf.Deg2Rad * 90;
+		var rotatedJumpVector = Vector3.RotateTowards(projectedJumpVector, Vector3.up, radiansToRotate, 0);
+		Debug.DrawRay(transform.position, rotatedJumpVector.normalized, Color.red);
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange); // acceleration is the worst change - constant velocity
