491a386730bf5358e4b3c8ec631c1a2d90b6f32b
AT <adriangtamas@gmail.com>
2017-07-19 01:23:46 +0300

    ST08 Scaling Down or Up

--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class FrogMovement : MonoBehaviour {
 
 	public float jumpElevationInDegrees = 45;
-	public float jumpSpeedInMPS = 5;
-	public float jumpGroundClearance = 2;
+	public float jumpSpeedInCMPS = 700;
+	public float jumpGroundClearance = 200;
 	public float jumpSpeedTolerance = 5;
 
 	// Use this for initialization
@@ -25,7 +25,7 @@ public class FrogMovement : MonoBehaviour {
 			var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up); //Debug.DrawRay(transform.position, projectedLookDirection, Color.blue);
 			var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
 			var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
-			var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS; //Debug.DrawRay(transform.position, jumpVector.normalized, Color.red);
+			var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInCMPS; //Debug.DrawRay(transform.position, jumpVector.normalized, Color.red);
 			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange); // acceleration is the worst change - constant velocity
 		}
 	}
