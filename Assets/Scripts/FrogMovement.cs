2527725a28c6f33a9e90391d25c1ec1f1a8fe24a
AT <adriangtamas@gmail.com>
2017-07-18 19:49:42 +0300

    ST04 - Added Movement with AddForce - avoided motion sickness

---
+++ Assets/Scripts/FrogMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogMovement : MonoBehaviour {
+
+	public Vector3 jumpVector = new Vector3(0,1,2);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange); // acceleration is the worst change - constant velocity
+		}
+	}
+}
