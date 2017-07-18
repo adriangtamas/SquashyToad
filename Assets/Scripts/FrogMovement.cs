21558f5547538c974d0182d535758164d1d70a29
AT <adriangtamas@gmail.com>
2017-07-18 22:02:15 +0300

    ST07 Jumping Only When Grounded

--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -5,6 +5,8 @@ public class FrogMovement : MonoBehaviour {
 
 	public float jumpElevationInDegrees = 45;
 	public float jumpSpeedInMPS = 5;
+	public float jumpGroundClearance = 2;
+	public float jumpSpeedTolerance = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -13,17 +15,17 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		var camera = GetComponentInChildren<Camera>();
-		Debug.DrawRay(transform.position, camera.transform.forward, Color.green);
-		//Debug.DrawRay(transform.position, jumpVector, Color.green);
-		var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
-		Debug.DrawRay(transform.position, projectedLookDirection, Color.blue);
-		var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
-		var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
-		var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS;
-		Debug.DrawRay(transform.position, jumpVector.normalized, Color.red);
-		if (Input.GetKeyDown(KeyCode.Space))
+		bool isOnGround = Physics.Raycast(transform.position, -transform.up, jumpGroundClearance);
+		Debug.DrawRay(transform.position, -transform.up * jumpGroundClearance);
+		var speed = GetComponent<Rigidbody>().velocity.magnitude;
+		bool isNearStationary = speed < jumpSpeedTolerance;
+		if ((Input.GetKeyDown(KeyCode.Space) || GvrViewer.Instance.Triggered) && isOnGround && isNearStationary)
 		{
+			var camera = GetComponentInChildren<Camera>(); //Debug.DrawRay(transform.position, camera.transform.forward, Color.green); //Debug.DrawRay(transform.position, jumpVector, Color.green);
+			var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up); //Debug.DrawRay(transform.position, projectedLookDirection, Color.blue);
+			var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
+			var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
+			var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS; //Debug.DrawRay(transform.position, jumpVector.normalized, Color.red);
 			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange); // acceleration is the worst change - constant velocity
 		}
 	}
