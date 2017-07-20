969ec267e73f5f79fdb2585fa9ae88ce5b4a3236
AT <adriangtamas@gmail.com>
2017-07-20 21:43:29 +0300

    ST13 Count-based Grounding Checks

--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -8,18 +8,31 @@ public class FrogMovement : MonoBehaviour {
 	public float jumpGroundClearance = 200;
 	public float jumpSpeedTolerance = 5;
 
+	public int collisionCount = 0;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	void OnCollisionEnter()
+	{
+		collisionCount++;
+	}
+
+	void OnCollisionExit()
+	{
+		collisionCount--;
+	} 
+
 	// Update is called once per frame
 	void Update () {
-		bool isOnGround = Physics.Raycast(transform.position, -transform.up, jumpGroundClearance);
-		Debug.DrawRay(transform.position, -transform.up * jumpGroundClearance);
-		var speed = GetComponent<Rigidbody>().velocity.magnitude;
-		bool isNearStationary = speed < jumpSpeedTolerance;
-		if ((Input.GetKeyDown(KeyCode.Space) || GvrViewer.Instance.Triggered) && isOnGround && isNearStationary)
+		//bool isOnGround = Physics.Raycast(transform.position, -transform.up, jumpGroundClearance);
+		bool isOnGround = collisionCount > 0;
+		//Debug.DrawRay(transform.position, -transform.up * jumpGroundClearance);
+		//var speed = GetComponent<Rigidbody>().velocity.magnitude;
+		//bool isNearStationary = speed < jumpSpeedTolerance;
+		if ((Input.GetKeyDown(KeyCode.Space) || GvrViewer.Instance.Triggered) && isOnGround)
 		{
 			var camera = GetComponentInChildren<Camera>(); //Debug.DrawRay(transform.position, camera.transform.forward, Color.green); //Debug.DrawRay(transform.position, jumpVector, Color.green);
 			var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up); //Debug.DrawRay(transform.position, projectedLookDirection, Color.blue);
